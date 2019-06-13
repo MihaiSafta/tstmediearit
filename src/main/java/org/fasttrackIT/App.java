36430e68f302ca6de7a38f785c316d35dafb2c9e
Mihai <mihaisaftas.259@gmail.com>
2019-06-13 14:45:32 +0300

    average of 3 numbers

--- src/main/java/org/fasttrackIT/App.java
+++ src/main/java/org/fasttrackIT/App.java
@@ -1,9 +1,11 @@
 package org.fasttrackIT;
 
-/**
- * Hello world!
- *
- */
+//         Exercitiu practic in clasa:
+//        1. Create a maven project for practicing algorithms (using maven-archetype-quickstart)
+//        2. Create a method in App to receive 3 numbers, display them and display their average.
+//        3. Invoke that method from App.main() with 3 numbers of your choice
+//        4. Share the project on GitHub.
+
 public class App 
 {
     public void displayAverage (double a, double b, double c){
